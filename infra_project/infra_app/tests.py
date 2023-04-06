0c7184f0d13b6b7bd1149de15107295d92496ae2
Сергей Коновалов <SokiZNosoK68rus@yandex.ru>
2023-04-06 18:31:00 +0300

    add tests

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -16,7 +16,7 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('/second/')
+        response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
@@ -24,5 +24,5 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
-        response = self.guest_client.get('/second/')
+        response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница')
